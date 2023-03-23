9e18034e317681d8f9e7d0607d88fd15b0dc78d9
Eduardo Frank <edfrank@gmail.com>
2023-03-23 21:49:21 +0100

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,9 +3,9 @@ defined('TYPO3') or die('Access denied.');
 /***************
  * Add default RTE configuration
  */
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['bca11'] = 'EXT:bca11/Configuration/RTE/BCA-RTE.yaml';
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['ddev_bca'] = 'EXT:ddev_bca/Configuration/RTE/BCA-RTE.yaml';
 
 /***************
  * PageTS
  */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:bca11/Configuration/TsConfig/Page/All.tsconfig">');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ddev_bca/Configuration/TsConfig/Page/All.tsconfig">');
